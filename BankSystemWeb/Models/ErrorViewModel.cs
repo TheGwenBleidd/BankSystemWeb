01fd4c9276e82635e19aefe9c7c3259f2d2ed72a
GwenBleidd <thegwyenbleidd@gmail.com>
2021-06-27 02:12:10 +0600

    Add project files.

---
+++ BankSystemWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BankSystemWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
