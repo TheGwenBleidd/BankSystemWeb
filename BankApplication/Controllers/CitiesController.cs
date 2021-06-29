88fcd689aef959378dc9e01b875b8efcf630356a
GwenBleidd <thegwyenbleidd@gmail.com>
2021-06-29 17:39:06 +0600

    added new methods

---
+++ BankApplication/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplication.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly banksystemdbContext _context;
+
+        public CitiesController(banksystemdbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var cities = _context.Cities.ToList();
+
+            return View(cities);
+        }
+    }
+}
---
+++ BankApplication/Controllers/CountriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplication.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly banksystemdbContext _context;
+
+        public CountriesController(banksystemdbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var countries = _context.Countries.ToList();
+
+            return View(countries);
+        }
+    }
+}
